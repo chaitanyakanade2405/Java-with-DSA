c200ddd1fb8472342c5270e45f73c34da9832c9e
chaitanyakanade2405 <chaitanyakanade2405@gmail.com>
2025-06-07 03:52:45 +0530

    solved ceiling and floor questions of binary search and solved 1 leetcode question

---
+++ Questions/floor.java
@@ -0,0 +1,28 @@
+package Questions;
+//condition: Find the largest element in the array that is less than or equal to the target value.
+// This is known as the "floor" of the target value in the array.
+public class floor {
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 34, 46, 58, 69, 78, 89, 99 };
+        int target = 54;
+        System.out.print(floor1(arr, target));
+    }
+
+    static int floor1(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
+
---
+++ Questions/smallestletter.java
@@ -0,0 +1,28 @@
+package Questions;
+//Find Smallest Letter Greater Than Target LeetCode problem #744
+//condition: find the smallest element in the array that is greater than the target
+//ceiling problem
+public class smallestletter {
+    public static void main(String[] args) {
+        char[] letters = {'c','f','j'};
+        char target = 'a';
+        System.out.print(nextGreatestLetter(letters, target));
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start% letters.length];
+    }
+    }
+
+    
