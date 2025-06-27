0ac141fd58234610a76db023bd9f4a55f1ebff97
chaitanyakanade2405 <chaitanyakanade2405@gmail.com>
2025-06-28 04:23:38 +0530

    Solved 2 LeetCode Problems on Arrays

---
+++ Questions/Array questions/TwoSum.java
@@ -0,0 +1,32 @@
+//Two Sum-2 Leetcode Problem #167 
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(numbers, target);
+        
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+    static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[] { left + 1, right + 1 }; 
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[] {};
+    }
+}
