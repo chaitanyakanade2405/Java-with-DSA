ea179841291616e97b1b406b4804cff942a33f3b
chaitanyakanade2405 <chaitanyakanade2405@gmail.com>
2025-07-31 19:49:40 +0530

    Solved 1 LeetCode Problem on Arrays

---
+++ Questions/Array questions/MatrixRotation.java
@@ -0,0 +1,50 @@
+// Rotate matrix by 90 degrees LeetCode Problem 48
+public class MatrixRotation {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("Rotated Matrix:");
+        printMatrix(matrix);
+    }
+
+    static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
